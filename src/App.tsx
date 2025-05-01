891ca0bf5d26f5b3e2d397bda803ba47dd5de778
Tak1za <varungupta2015135@gmail.com>
2025-05-01 21:18:01 +0530

    added basic layout

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import Layout from "@/components/layout";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Layout>
+        <div className="flex flex-col pb-2 px-2">
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+          <div>content goes here</div>
+        </div>
+      </Layout>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "./mode-toggle";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        <div className="flex justify-between sticky top-0 z-10 bg-background pt-2 pr-2">
+          <SidebarTrigger />
+          <ModeToggle />
+        </div>
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
